[DEBUG] use weighted normals to partially fix sign computation

tests/normal.py
@@ -24,12 +24,16 @@ for model in os.listdir("tests/models"):
     verts = torch.Tensor(mesh.vertices.copy()).to(device)
     # (Nf, 3)
     faces = torch.Tensor(mesh.faces.copy()).long().to(device)
+    # (Nv, 3)
+    vert_normals = torch.Tensor(mesh.vertex_normals.copy()).to(device)
     # (Nf, 3, 3)
     face_verts = index_vertices_by_faces(verts, faces)
+    # (Nf, 3, 3)
+    face_vert_normals = index_vertices_by_faces(vert_normals, faces)
 
     # TorchSDF
     # (Ns)
-    distances, dist_sign, normals, clst_points = compute_sdf(x, face_verts)
+    distances, dist_sign, normals, clst_points = compute_sdf(x, face_verts, face_vert_normals)
     gradient = torch.autograd.grad([distances.sum()], [x], create_graph=True,
                                    retain_graph=True)[0]
 

tests/speed.py
@@ -25,11 +25,15 @@ for model in os.listdir("tests/models"):
     verts = torch.Tensor(mesh.vertices.copy()).to(device)
     # (Nf, 3)
     faces = torch.Tensor(mesh.faces.copy()).long().to(device)
+    # (Nv, 3)
+    vert_normals = torch.Tensor(mesh.vertex_normals.copy()).to(device)
     # (1, Nf, 3, 3)
     face_verts = kaolin.ops.mesh.index_vertices_by_faces(
         verts.unsqueeze(0), faces)
     # (Nf, 3, 3)
     face_verts_ts = index_vertices_by_faces(verts, faces)
+    # (Nf, 3, 3)
+    face_vert_normals = index_vertices_by_faces(vert_normals, faces)
 
     # Kaolin
     # (1, Ns)
@@ -50,14 +54,14 @@ for model in os.listdir("tests/models"):
     torch.cuda.synchronize()
     tmp = time()
     distances_ts, dist_sign_ts, normals_ts, clst_points_ts = compute_sdf(
-        x, face_verts_ts)
+        x, face_verts_ts, face_vert_normals)
     sdf_ts = distances_ts.sqrt() * dist_sign_ts
     torch.cuda.synchronize()
     time_ts = time() - tmp
-    
+
     equal_num = (dist_sign_ts == signs).sum().item()
     equal_ratio = equal_num/num_sample
-    sign_fit = (equal_ratio > 0.98)
+    sign_fit = (equal_ratio > 0.99)
     dis_fit = torch.allclose(distances, distances_ts)
     if (dis_fit and sign_fit):
         print("\x1B[32mPass\x1B[0m")

tests/value.py
@@ -25,16 +25,20 @@ for model in os.listdir("tests/models"):
     verts = torch.Tensor(mesh.vertices.copy()).to(device)
     # (Nf, 3)
     faces = torch.Tensor(mesh.faces.copy()).long().to(device)
+    # (Nv, 3)
+    vert_normals = torch.Tensor(mesh.vertex_normals.copy()).to(device)
     # (1, Nf, 3, 3)
     face_verts = kaolin.ops.mesh.index_vertices_by_faces(
         verts.unsqueeze(0), faces)
     # (Nf, 3, 3)
     face_verts_ts = index_vertices_by_faces(verts, faces)
+    # (Nf, 3, 3)
+    face_vert_normals = index_vertices_by_faces(vert_normals, faces)
 
     # Kaolin
-    # (1, Ns)
     torch.cuda.synchronize()
     tmp = time()
+    # (1, Ns)
     distances, face_indexes, types = kaolin.metrics.trianglemesh.point_to_mesh_distance(
         x.unsqueeze(0), face_verts)
     gradient = torch.autograd.grad([distances.sum()], [x], create_graph=True,
@@ -43,11 +47,11 @@ for model in os.listdir("tests/models"):
     time_kaolin = time() - tmp
 
     # TorchSDF
-    # (Ns)
     torch.cuda.synchronize()
     tmp = time()
+    # (Ns)
     distances_ts, dist_sign_ts, normals_ts, clst_points_ts = compute_sdf(
-        x, face_verts_ts)
+        x, face_verts_ts, face_vert_normals)
     gradient_ts = torch.autograd.grad([distances_ts.sum()], [x], create_graph=True,
                                       retain_graph=True)[0]
     torch.cuda.synchronize()

torchsdf/sdf.py
@@ -31,13 +31,13 @@ def index_vertices_by_faces(vertices_features, faces):
     return torch.gather(input=input, index=indices, dim=0)
 
 
-def compute_sdf(pointclouds, face_vertices):
-    return _UnbatchedTriangleDistanceCuda.apply(pointclouds, face_vertices)
+def compute_sdf(pointclouds, face_vertices, face_vert_normals):
+    return _UnbatchedTriangleDistanceCuda.apply(pointclouds, face_vertices, face_vert_normals)
 
 
 class _UnbatchedTriangleDistanceCuda(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, points, face_vertices):
+    def forward(ctx, points, face_vertices, face_vert_normals):
         num_points = points.shape[0]
         min_dist = torch.zeros(
             (num_points), device=points.device, dtype=points.dtype)
@@ -48,7 +48,7 @@ class _UnbatchedTriangleDistanceCuda(torch.autograd.Function):
         clst_points = torch.zeros(
             (num_points, 3), device=points.device, dtype=points.dtype)
         _C.unbatched_triangle_distance_forward_cuda(
-            points, face_vertices, min_dist, dist_sign, normals, clst_points)
+            points, face_vertices, face_vert_normals, min_dist, dist_sign, normals, clst_points)
         ctx.save_for_backward(points.contiguous(), clst_points)
         ctx.mark_non_differentiable(dist_sign, normals, clst_points)
         return min_dist, dist_sign, normals, clst_points
@@ -59,6 +59,7 @@ class _UnbatchedTriangleDistanceCuda(torch.autograd.Function):
         grad_dist = grad_dist.contiguous()
         grad_points = torch.zeros_like(points)
         grad_face_vertices = None
+        grad_face_vert_normals = None
         _C.unbatched_triangle_distance_backward_cuda(
             grad_dist, points, clst_points, grad_points)
-        return grad_points, grad_face_vertices
+        return grad_points, grad_face_vertices, grad_face_vert_normals